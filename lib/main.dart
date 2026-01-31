e69c938936a3ff6e31a445b1e6fb26cd041c09d9
priyanshu <negip4484@gmail.com>
2026-01-31 21:14:09 +0530

    set up completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:final_year_project/components/bottom_navigation_bar/my_bottom_navigation_bar.dart';
+import 'package:final_year_project/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
@@ -19,7 +24,6 @@ class MyApp extends StatelessWidget {
         textTheme: GoogleFonts.latoTextTheme(),
       ),
       home: MyBottomNavigationBar(),
-      
     );
   }
 }
